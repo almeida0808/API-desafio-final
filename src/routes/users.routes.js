239b652144a1b4db98c55eca0feb977b15ab3767
almeida0808 <almeidaff08@mail.com>
2024-07-04 11:04:14 -0300

    função update

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,5 +5,6 @@ const UsersController = require("../Controllers/UsersController/UsersController"
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create); // quando acessar a raiz do /users e fizer uma requisição no metodo post, ele vai realizar a função que for colocado depois da virgula(ainda não criamos a função)
+usersRoutes.put("/:user_id", usersController.update);
 
 module.exports = usersRoutes;
